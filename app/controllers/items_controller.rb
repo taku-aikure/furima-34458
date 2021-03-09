dcbb31fab0ea01e37e1ea79f97758453b740e098
taku-aikure <aikure.raven14@gmail.com>
2021-03-09 16:04:19 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item, only: [:show, :edit ]
+  before_action :move_to_root, only: [:edit, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -16,16 +17,36 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
     def show
-      @item = Item.find(params[:id])
     end
-  end
+
+    def edit
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    end
+
 
   private
   
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :cost_id, :area_id, :days_id, :price, :image ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_root
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
